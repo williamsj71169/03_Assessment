73354fbea2545ecc01f3ce7afe06c0add25d9df6
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2022-06-15 12:20:04 +1200

    15.6.22

--- v1/08_help_and_stats.py
+++ v1/08_help_and_stats.py
@@ -1,5 +1,5 @@
 from tkinter import *
-# from functools import partial  # to prevent unwanted windows
+from functools import partial  # to prevent unwanted windows
 import csv
 import random
 
@@ -176,6 +176,7 @@ class Game:
         # Help button
         self.help_button = Button(self.export_help_frame, text="Help / Rules",
                                   font="Arial 15 bold",
+                                  command=lambda: self.to_help,
                                   bg="#9233ff", fg="white")
         self.help_button.grid(row=0, column=0, padx=2)
 
@@ -260,6 +261,57 @@ class Game:
 
     def to_quit(self):
         root.destroy()
+
+    def to_help(self):
+        get_help = Help(self)
+
+
+class Help:
+    def __init__(self, partner):
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # sets up child window
+        self.help_box = Toplevel()
+
+        # if user press cross at top closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # set up gui frame
+        self.help_frame = Frame(self.help_box, width=300)
+        self.help_frame.grid()
+
+        # set up heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help / Instructions",
+                                 font="arial 14 bold")
+        self.how_heading.grid(row=0)
+
+        help_text = "Choose and amount to play with and then choose the stakes." \
+                    "Higher stakes cost more per round but you can win more as well.\n\n" \
+                    "When you enter the play area, you will see three mystery boxes. To reveal" \
+                    "the contents of the boxes, click the 'Open Boxes' button. If you don't have " \
+                    "enough money to play, the button will turn red and you need to quit the game.\n\n" \
+                    "The contents of the boxes will be added to your balance. The boxes could contain...\n\n" \
+                    "Low: Lead($0) | Copper ($1) | Silver($2) | Gold ($10)\n" \
+                    "Medium: Lead($0) | Copper ($2) | Silver($4) | Gold ($25)\n" \
+                    "High: Lead($0) | Copper ($5) | Silver($10) | Gold ($50)\n\n" \
+\
+            # help text (label, row 1)
+        self.help_text = Label(self.help_frame, text=help_text,
+                               justify=LEFT, wrap=400, padx=10, pady=10)
+        self.help_text.grid(row=1)
+
+        # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="#660000", fg="white",
+                                  font="arial 15 bold", command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=3, pady=10)
+
+    def close_help(self, partner):
+        # put calc history button back to normal
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
 
 # main routine
 if __name__ == "__main__":
